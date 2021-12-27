c77de79b270b5d9761b8cda81a94044179e02109
Mikael Jansson <92028583+heymikeey@users.noreply.github.com>
2021-12-27 17:06:02 +0100

    added player

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,30 @@
 import javax.swing.*;
+import javax.swing.plaf.synth.SynthScrollBarUI;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Main implements ActionListener {
-    public static String player1FirstCard = null;
-
+    String firstCard = null;
+    String secondCard = null;
 
     public static void main(String[] args) {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Buttons gui = new Buttons();
 
-        SwingUtilities.invokeLater(new LaunchMenu());
+        boolean gameOver = false;
 
 
     }
+    
+    
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
     }
+    
+    
+    
 }
---
+++ src/Player.java
@@ -0,0 +1,8 @@
+public class Player {
+    boolean playersTurn = true;
+    int cardsSelected = 0;
+    String firstCard = null;
+    String secondCard = null;
+    
+    int points = 0;
+}
