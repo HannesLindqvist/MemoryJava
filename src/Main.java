81ade1708d7af26ee95c8f061ef74c088c3d951e
Josip <josip840@gmail.com>
2021-12-27 11:55:00 +0100

    fixed conflict

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,8 @@ public class Main implements ActionListener {
 
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        SwingUtilities.invokeLater(new LaunchMenu());
-=======
-        SwingUtilities.invokeLater(new MenuPage());
 
->>>>>>> 66dbee4db230dd2a2d07723439c10f2d9dc0f64b
+        SwingUtilities.invokeLater(new LaunchMenu());
 
     }
 
